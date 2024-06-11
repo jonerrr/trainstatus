dc988f9ce62f87de42c85bf21be4dd7da634801d
Jonah <73760377+jonerrr@users.noreply.github.com>
2024-06-11 17:45:22 -0400

    disable swiper auto height and lower stop list transition speed

--- frontend/src/lib/components/RouteAlert/Content.svelte
+++ frontend/src/lib/components/RouteAlert/Content.svelte
@@ -22,10 +22,12 @@
 	});
 
 	function fix_swiper({ detail }: { detail: [Swiper] }) {
+		console.log('fixing swiper');
+
 		const swiper = detail[0];
-		setTimeout(() => {
+		setTimeout(async () => {
 			console.log('fixing swiper');
-			// swiper.update();
+			swiper.update();
 			// swiper.slideReset();
 		}, 500);
 	}
@@ -33,50 +35,53 @@
 
 <!-- TODO: fix swiper slides breaking for certain alerts (i think i need to somehow update the component) -->
 <!-- it seems to fix itself when clicking outside of the dialog (but that closes it) -->
-<div class="max-h-[80vh]">
-	{#if $route_alerts.length}
-		<swiper-container
-			style="--swiper-pagination-bullet-inactive-color: #0a0a0a; --swiper-pagination-color: #6366f1;"
-			pagination="true"
-			auto-height="true"
-			loop={$route_alerts.length > 1}
-			on:swiperinit={fix_swiper}
-		>
-			{#each $route_alerts as alert}
-				<swiper-slide class="">
-					<div class="">
-						<h2 class="sticky top-0 font-bold flex items-center gap-2 text-indigo-300">
-							<Icon width="2rem" height="2rem" name={route_id} />
-							{alert.alert_type}
-						</h2>
+<!-- <div class="max-h-[80vh]"> -->
+{#if $route_alerts.length}
+	<swiper-container
+		style="--swiper-pagination-bullet-inactive-color: #0a0a0a; --swiper-pagination-color: #6366f1;"
+		pagination="true"
+		auto-height="false"
+		observer="true"
+		observe-parents="true"
+		loop={$route_alerts.length > 1}
+		on:swiperinit={fix_swiper}
+	>
+		{#each $route_alerts as alert}
+			<swiper-slide>
+				<div class="relative flex flex-col max-h-[80vh]">
+					<h2 class="sticky top-0 font-bold flex items-center gap-2 text-indigo-300">
+						<Icon width="2rem" height="2rem" name={route_id} />
+						{alert.alert_type}
+					</h2>
 
-						<div class="text-indigo-200 overflow-auto border border-neutral-700 rounded my-2">
-							<!-- hypothetically, the MTA could XSS this website (that would be silly) -->
-							{@html alert.header_html}
-							<!-- TODO: remove links or mark them as links -->
-							{#if alert.description_html}
-								{@html alert.description_html}
-							{/if}
-						</div>
-
-						<div class="text-sm text-neutral-400 flex justify-between">
-							<div>Updated {dayjs(alert.updated_at).fromNow()}</div>
-							{#if alert.end_time}
-								<!-- TODO: get the earliest end_time from API -->
-								<div>End {dayjs(alert.end_time).fromNow()}</div>
-							{/if}
-						</div>
+					<div class="text-indigo-200 overflow-auto border border-neutral-700 rounded my-2">
+						<!-- hypothetically, the MTA could XSS this website (that would be silly) -->
+						{@html alert.header_html}
+						<!-- TODO: remove links or mark them as links -->
+						{#if alert.description_html}
+							{@html alert.description_html}
+						{/if}
 					</div>
-				</swiper-slide>
-			{/each}
-		</swiper-container>
-	{:else}
-		<h2 class="sticky top-0 font-bold flex items-center gap-2 text-indigo-300">
-			<Icon width="2rem" height="2rem" name={route_id} />
-		</h2>
-		<div class="text-indigo-200">No alerts</div>
-	{/if}
-</div>
+
+					<div class="text-sm text-neutral-400 flex justify-between">
+						<div>Updated {dayjs(alert.updated_at).fromNow()}</div>
+						{#if alert.end_time}
+							<!-- TODO: get the earliest end_time from API -->
+							<div>End {dayjs(alert.end_time).fromNow()}</div>
+						{/if}
+					</div>
+				</div>
+			</swiper-slide>
+		{/each}
+	</swiper-container>
+{:else}
+	<h2 class="sticky top-0 font-bold flex items-center gap-2 text-indigo-300">
+		<Icon width="2rem" height="2rem" name={route_id} />
+	</h2>
+	<div class="text-indigo-200">No alerts</div>
+{/if}
+
+<!-- </div> -->
 
 <style lang="postcss">
 	swiper-container::part(pagination) {
