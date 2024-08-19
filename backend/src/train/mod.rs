c0b66262cafc1bf54531551d73de6543c96bae39
Jonah <73760377+jonerrr@users.noreply.github.com>
2024-08-18 22:36:45 -0400

    test enum

--- backend/src/train/mod.rs
+++ backend/src/train/mod.rs
@@ -1,2 +1,18 @@
 pub mod static_data;
 pub mod trips;
+
+// pub struct Trip {
+//     id: Uuid,
+//     mta_id: String,
+//     created_at: DateTime<Utc>,
+//     direction: Direction,
+// }
+
+// pub enum Direction {
+//     Northbound,
+//     Southbound,
+// }
+
+// pub enum Bus {
+//     Train { id: String },
+// }
