c4996eec09c4842a8f13cb1742fd5e90e5c743b1
Jonah <73760377+jonerrr@users.noreply.github.com>
2024-09-10 11:44:25 -0400

    work on websocket

--- backend/src/api/realtime.rs
+++ backend/src/api/realtime.rs
@@ -1,24 +1,23 @@
-use axum::{
-    extract::ws::{Message, WebSocket, WebSocketUpgrade},
-    extract::Extension,
-    routing::get,
-    Router,
+use crate::AppState;
+use axum::extract::{
+    ws::{Message, WebSocket, WebSocketUpgrade},
+    Extension, State,
 };
 use futures::{sink::SinkExt, stream::StreamExt};
 use serde::{Deserialize, Serialize};
 use std::collections::{HashMap, HashSet};
-use std::sync::{Arc, Mutex};
-use tokio::sync::broadcast;
-use tokio::task;
+use std::sync::Arc;
+use tokio::sync::{broadcast, Mutex};
 
-type Clients = Arc<Mutex<HashMap<String, HashSet<String>>>>;
+pub type Clients = Arc<Mutex<HashMap<String, HashSet<String>>>>;
 
 pub async fn realtime_handler(
     ws: WebSocketUpgrade,
-    Extension(clients): Extension<Clients>,
-    Extension(tx): Extension<broadcast::Sender<Update>>,
+    // Extension(clients): Extension<Clients>,
+    // Extension(tx): Extension<crossbeam::channel::Sender<serde_json::Value>>,
+    State(state): State<AppState>,
 ) -> impl axum::response::IntoResponse {
-    ws.on_upgrade(move |socket| handle_socket(socket, clients, tx))
+    ws.on_upgrade(move |socket| handle_socket(socket, state.clients, state.rx, state.initial_data))
 }
 
 #[derive(Deserialize)]
@@ -29,38 +28,189 @@ pub struct FollowingData {
 #[derive(Clone)]
 pub struct Update {
     pub route_id: String,
+    pub data_type: String,
     pub data: serde_json::Value,
 }
 
-async fn handle_socket(mut socket: WebSocket, clients: Clients, tx: broadcast::Sender<Update>) {
-    let mut followed_routes = HashSet::new();
+async fn handle_socket(
+    socket: WebSocket,
+    clients: Clients,
+    // tx: crossbeam::channel::Sender<serde_json::Value>,
+    rx: crossbeam::channel::Receiver<Vec<Update>>,
+    initial_data: Arc<Mutex<serde_json::Value>>,
+) {
+    // TODO: maybe switch to message::binary
+    let (mut sender, mut receiver) = socket.split();
 
-    while let Some(Ok(message)) = socket.next().await {
-        if let Message::Text(text) = message {
-            if let Ok(follow_routes) = serde_json::from_str::<FollowingData>(&text) {
-                followed_routes = follow_routes.route_ids.into_iter().collect();
-                let client_id = uuid::Uuid::now_v7().to_string();
-                clients
-                    .lock()
-                    .unwrap()
-                    .insert(client_id.clone(), followed_routes.clone());
+    sender
+        .send(Message::Text(initial_data.lock().await.to_string()))
+        .await
+        .unwrap();
 
-                let clients = clients.clone();
-                let mut socket = socket;
-                task::spawn(async move {
-                    let mut rx = tx.clone().subscribe();
+    let client_id = uuid::Uuid::now_v7().to_string();
+    clients
+        .lock()
+        .await
+        .insert(client_id.clone(), HashSet::new());
 
-                    while let Ok(update) = rx.recv().await {
-                        if followed_routes.contains(&update.route_id) {
-                            let msg = serde_json::to_string(&update.data).unwrap();
-                            if socket.send(Message::Text(msg)).await.is_err() {
-                                break;
-                            }
-                        }
+    let recv_clients = clients.clone();
+    let recv_client_id = client_id.clone();
+    let mut recv_task = tokio::spawn(async move {
+        while let Some(Ok(msg)) = receiver.next().await {
+            match msg {
+                Message::Text(text) => {
+                    if let Ok(follow_routes) = serde_json::from_str::<FollowingData>(&text) {
+                        // dbg!(&follow_routes);
+
+                        let mut clients = recv_clients.lock().await;
+
+                        clients.insert(
+                            recv_client_id.clone(),
+                            follow_routes.route_ids.into_iter().collect(),
+                        );
                     }
-                    clients.lock().unwrap().remove(&client_id);
-                });
+                }
+                _ => {}
             }
         }
-    }
+    });
+
+    // let mut rx = rx.clone();
+    let send_clients = clients.clone();
+    let mut send_task = tokio::spawn(async move {
+        while let Ok(update) = rx.recv() {
+            let clients = send_clients.lock().await;
+
+            for (_client_id, followed_routes) in clients.iter() {
+                let updates = update
+                    .iter()
+                    .filter_map(|u| {
+                        // we only send specific stop_times for buses. For train we send all
+                        if u.data_type == "train" || followed_routes.contains(&u.route_id) {
+                            Some(&u.data)
+                        } else {
+                            None
+                        }
+                    })
+                    .collect::<Vec<_>>();
+
+                if updates.is_empty() {
+                    continue;
+                }
+
+                let msg = serde_json::to_string(&updates).unwrap();
+                if sender.send(Message::Text(msg)).await.is_err() {
+                    break;
+                }
+            }
+
+            // for (client_id, followed_routes) in clients.iter() {
+            //     if followed_routes.contains(&update.route_id) {
+            //         let msg = serde_json::to_string(&update.data).unwrap();
+            //         if sender.send(Message::Text(msg)).await.is_err() {
+            //             break;
+            //         }
+            //     }
+            // }
+        }
+    });
+
+    tokio::select! {
+        _ = &mut send_task => recv_task.abort(),
+        _ = &mut recv_task => send_task.abort(),
+    };
+
+    // remove client from clients when recv_task is done
+    clients.lock().await.remove(&client_id);
+
+    // let mut followed_routes = HashSet::new();
+
+    // let mut recv_task = tokio::spawn(async move {
+    //     // let mut cnt = 0;
+    //     while let Some(Ok(msg)) = receiver.next().await {
+    //         dbg!(&msg);
+
+    //         match msg {
+    //             Message::Text(text) => {
+    //                 if let Ok(follow_routes) = serde_json::from_str::<FollowingData>(&text) {
+    //                     let mut clients = clients.lock().await;
+    //                     let client_id = uuid::Uuid::now_v7().to_string();
+    //                     clients.insert(client_id, follow_routes.route_ids.into_iter().collect());
+
+    //                     // let clients = clients.clone();
+
+    //                     // let tx = tx.clone();
+    //                     // tokio::spawn(async move {
+    //                     //     let mut rx = tx.subscribe();
+
+    //                     //     while let Ok(update) = rx.recv().await {
+    //                     //         if clients.get(&client_id).unwrap().contains(&update.route_id) {
+    //                     //             let msg = serde_json::to_string(&update.data).unwrap();
+    //                     //             if sender.send(Message::Text(msg)).await.is_err() {
+    //                     //                 break;
+    //                     //             }
+    //                     //         }
+    //                     //     }
+    //                     //     clients.remove(&client_id);
+    //                     // });
+    //                 }
+    //             }
+    //             // Message::Close(_) => break,
+    //             _ => {}
+    //         }
+
+    //         // cnt += 1;
+    //         // // print message and break if instructed to do so
+    //         // if process_message(msg, who).is_break() {
+    //         //     break;
+    //         // }
+    //     }
+    //     // cnt
+    // })
+    // .await;
+
+    // tokio::select! {
+    //     _ = recv_task => {
+    //         // println!("{} received {} messages", who, cnt);
+    //     }
+    //     _ = async {
+    //         let mut rx = tx.subscribe();
+    //         while let Ok(update) = rx.recv().await {
+    //             if sender.send(Message::Text(update.data.to_string())).await.is_err() {
+    //                 break;
+    //             }
+    //         }
+    //     } => {
+    //         // println!("{} sent {} messages", who, cnt);
+    //     }
+    // }
+
+    // while let Some(Ok(message)) = socket.next().await {
+    //     if let Message::Text(text) = message {
+    //         if let Ok(follow_routes) = serde_json::from_str::<FollowingData>(&text) {
+    //             followed_routes = follow_routes.route_ids.into_iter().collect();
+    //             let client_id = uuid::Uuid::now_v7().to_string();
+    //             clients
+    //                 .lock()
+    //                 .unwrap()
+    //                 .insert(client_id.clone(), followed_routes.clone());
+
+    //             let clients = clients.clone();
+    //             let tx = tx.clone();
+    //             task::spawn(async move {
+    //                 let mut rx = tx.subscribe();
+
+    //                 while let Ok(update) = rx.recv().await {
+    //                     if followed_routes.contains(&update.route_id) {
+    //                         let msg = serde_json::to_string(&update.data).unwrap();
+    //                         if socket.send(Message::Text(msg)).await.is_err() {
+    //                             break;
+    //                         }
+    //                     }
+    //                 }
+    //                 clients.lock().unwrap().remove(&client_id);
+    //             });
+    //         }
+    //     }
+    // }
 }
--- backend/src/realtime/mod.rs
+++ backend/src/realtime/mod.rs
@@ -1,5 +1,7 @@
+use crate::api::realtime::Update;
 use crate::{feed::FeedMessage, train::trips::DecodeFeedError};
 use chrono::Utc;
+use crossbeam::channel::Sender;
 use prost::Message;
 use rayon::prelude::*;
 use serde_json::json;
@@ -8,7 +10,6 @@ use std::collections::HashSet;
 use std::sync::{Arc, OnceLock};
 use std::{env::var, time::Duration};
 use thiserror::Error;
-use tokio::sync::broadcast::Sender;
 use tokio::sync::Mutex;
 use tokio::{
     fs::{create_dir, write},
@@ -60,7 +61,8 @@ pub enum ImportError {
 pub async fn import(
     pool: PgPool,
     updated_trips: Arc<Mutex<Vec<trip::Trip>>>,
-    tx: Sender<serde_json::Value>,
+    tx: Sender<Vec<Update>>,
+    initial_data: Arc<Mutex<serde_json::Value>>,
 ) {
     let t_pool = pool.clone();
     let b_pool = pool.clone();
@@ -72,7 +74,7 @@ pub async fn import(
             let _ = alert::import(&pool)
                 .await
                 .inspect_err(|e| tracing::error!("alert::import: {}", e));
-            sleep(Duration::from_secs(30)).await;
+            sleep(Duration::from_secs(35)).await;
         }
     });
 
@@ -81,7 +83,7 @@ pub async fn import(
             let _ = train::import(&t_pool, updated_trips.clone())
                 .await
                 .inspect_err(|e| tracing::error!("train::import: {}", e));
-            sleep(Duration::from_secs(30)).await;
+            sleep(Duration::from_secs(35)).await;
         }
     });
 
@@ -116,11 +118,32 @@ pub async fn import(
             HashSet::from_par_iter(last_stop_times.into_par_iter());
 
         loop {
-            let Ok(stop_times) = stop_time::StopTime::get_all(&c_pool, Utc::now()).await else {
-                tracing::error!("failed to get stop times");
-                sleep(Duration::from_secs(30)).await;
+            let Ok(trips) = trip::Trip::get_all(&c_pool, Utc::now()).await else {
+                tracing::error!("failed to get trips");
+                sleep(Duration::from_secs(35)).await;
                 continue;
             };
+
+            let Ok(stop_times) = stop_time::StopTime::get_all(&c_pool, Utc::now()).await else {
+                tracing::error!("failed to get stop times");
+                sleep(Duration::from_secs(35)).await;
+                continue;
+            };
+
+            let Ok(alerts) = alert::Alert::get_all(&c_pool, Utc::now()).await else {
+                tracing::error!("failed to get alerts");
+                sleep(Duration::from_secs(35)).await;
+                continue;
+            };
+
+            // update initial data
+            let mut initial_data = initial_data.lock().await;
+            *initial_data = json!({
+                "trips": trips,
+                "alerts": alerts,
+                "stop_times": stop_times.iter().filter(|st| st.trip_type == "train").collect::<Vec<_>>(),
+            });
+            drop(initial_data);
 
             let stop_times_set: HashSet<_> = HashSet::from_par_iter(stop_times.into_par_iter());
 
@@ -136,15 +159,33 @@ pub async fn import(
             if !changed_stop_times.is_empty() || !removed_stop_times.is_empty() {
                 dbg!(changed_stop_times.len());
 
-                let data = json!({
-                    "changed_stop_times": changed_stop_times,
-                    "removed_stop_times": removed_stop_times,
-                });
-                let _ = tx.send(data);
+                // let data = json!({
+                //     "changed_stop_times": changed_stop_times,
+                //     "removed_stop_times": removed_stop_times,
+                // });
+                let updates = changed_stop_times
+                    .into_iter()
+                    .map(|st| Update {
+                        route_id: st.route_id.clone(),
+                        data_type: st.trip_type.clone(),
+                        data: serde_json::to_value(st).unwrap(),
+                    })
+                    .collect::<Vec<_>>();
+
+                let _ = tx.send(updates);
 
                 // Update the last stop times
                 last_stop_times_set = stop_times_set;
             }
+
+            // // update initial data
+            // let mut initial_data = initial_data.lock().await;
+
+            // TODO: don't unwrap
+            // let current_trips = trip::Trip::get_all(&c_pool, Utc::now()).await.unwrap();
+            // let stop_times = stop_time::StopTime::get_all(&c_pool, Utc::now())
+            //     .await
+            //     .unwrap();
 
             // // get the changed stop times
             // let changed_stop_times = stop_times
@@ -160,7 +201,7 @@ pub async fn import(
             //     let _ = tx.send(data);
             // }
 
-            sleep(Duration::from_secs(30)).await;
+            sleep(Duration::from_secs(35)).await;
         }
     });
 }
