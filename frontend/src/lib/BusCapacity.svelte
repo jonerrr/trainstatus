e335d6755939608978371036e922880dcdcfa944
Jonah <73760377+jonerrr@users.noreply.github.com>
2024-10-05 20:38:15 -0400

    work on search and bus data in modal

---
+++ frontend/src/lib/BusCapacity.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Users } from 'lucide-svelte';
+
+	const { passengers, capacity }: { passengers: number; capacity: number } = $props();
+
+	const p_full = $derived(Math.floor((passengers / capacity) * 100));
+</script>
+
+<div
+	class={`flex items-center gap-1 text-neutral-200 ${p_full > 30 ? 'text-yellow-400' : ''} ${p_full > 80 ? 'text-red-400' : ''}`}
+>
+	<Users size="16" />
+	{passengers}
+</div>
