8155ca53a42014753889b4b57d6e8a70340d2332
Jonah <73760377+jonerrr@users.noreply.github.com>
2024-12-07 12:07:04 -0500

    fix route stops positioning and use dialog element for modal

--- frontend/src/lib/Modal.svelte
+++ frontend/src/lib/Modal.svelte
@@ -18,23 +18,49 @@
 	import Pin from './Pin.svelte';
 	import { is_bus_route, type Trip, type TripData } from './trips.svelte';
 
+	// let last_focused: HTMLElement | null = null;
+
+	let dialog_el = $state<HTMLDialogElement>();
+
 	function close() {
 		// enable_scroll();
 		pushState('', { modal: null });
 	}
 
-	function manage_modal(node: HTMLDivElement) {
+	function manage_modal(node: HTMLDialogElement) {
 		document.body.style.overflow = 'hidden';
+		// last_focused = document.activeElement as HTMLElement;
 
-		// page.subscribe(({ state }) => {
-		// 	if (state.modal) {
-		// 		document.body.style.overflow = 'hidden';
-		// 		// disable_scroll();
-		// 	} else {
-		// 		document.body.style.overflow = '';
-		// 		// enable_scroll();
-		// 	}
-		// });
+		function trap_focus(event: KeyboardEvent) {
+			if (event.key === 'Tab') {
+				const focusable = Array.from(
+					node.querySelectorAll(
+						'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+					)
+				) as HTMLElement[];
+				const first = focusable[0];
+				const last = focusable[focusable.length - 1];
+
+				if (event.shiftKey && document.activeElement === first) {
+					last.focus();
+					event.preventDefault();
+				} else if (!event.shiftKey && document.activeElement === last) {
+					first.focus();
+					event.preventDefault();
+				}
+			}
+		}
+
+		// document.addEventListener('keydown', trap_focus);
+
+		// watch for clicks outside the dialog to close it
+		function handle_click(event: MouseEvent) {
+			if (event.target === node) {
+				close();
+			}
+		}
+
+		node.addEventListener('click', handle_click);
 
 		// This differentiates between a drag and a click so mobile users don't accidentally close the dialog when swiping to go back
 		// from here https://stackoverflow.com/a/59741870
@@ -69,19 +95,29 @@
 				document.body.style.overflow = '';
 				node.removeEventListener('mousedown', handle_mouse_down);
 				node.removeEventListener('mouseup', handle_mouse_up);
+				node.removeEventListener('click', handle_click);
+				// document.removeEventListener('keydown', trap_focus);
+
+				// if (last_focused) {
+				// 	last_focused.focus();
+				// }
 			}
 		};
 	}
 
 	// manage title changes and monitored bus routes
 	onMount(() => {
-		page.subscribe(({ state, route }) => {
+		const unsubscribe = page.subscribe(({ state, route }) => {
 			// console.log(route, state.modal);
 			switch (state.modal) {
 				case 'route':
+					dialog_el?.showModal();
+
 					document.title = `Alerts for ${state.data.id}`;
 					break;
 				case 'stop':
+					dialog_el?.showModal();
+
 					document.title = `Arrivals at ${state.data.name}`;
 
 					const stop: Stop<'bus' | 'train'> = state.data;
@@ -91,6 +127,8 @@
 					}
 					break;
 				case 'trip':
+					dialog_el?.showModal();
+
 					document.title = `${state.data.route_id} Trip`;
 
 					const trip: Trip<TripData> = state.data;
@@ -102,6 +140,7 @@
 
 					break;
 				default:
+					dialog_el?.close();
 					switch (route.id) {
 						case '/stops':
 							document.title = 'Stops';
@@ -116,6 +155,10 @@
 					break;
 			}
 		});
+
+		return () => {
+			unsubscribe();
+		};
 	});
 
 	let copied = $state(false);
@@ -209,53 +252,53 @@
 {/snippet}
 
 <!-- close modal on escape key -->
-<svelte:window onkeydown={($event) => $page.state.modal && $event.key == 'Escape' && close()} />
+<!-- <svelte:window onkeydown={($event) => $page.state.modal && $event.key == 'Escape' && close()} /> -->
 
-{#if $page.state.modal}
+<!-- {#if $page.state.modal}
 	<div
 		use:manage_modal
 		class="fixed top-0 left-0 flex flex-col justify-center items-center w-[100dvw] h-[100dvh] z-50 bg-black/50 bg-opacity-10 text-neutral-100"
-	>
-		<!-- transition:slide={{ duration: 150 }} -->
-		<div
-			role="dialog"
-			aria-modal="true"
-			class="snap-start bg-neutral-900 w-full md:w-[60%] rounded flex flex-col fixed bottom-0"
-		>
-			{#if $page.state.modal === 'stop'}
-				<StopModal {show_previous} time_format={time_format.value} stop={$page.state.data} />
+	> -->
+<!-- transition:slide={{ duration: 150 }} -->
+<dialog
+	bind:this={dialog_el}
+	use:manage_modal
+	class="text-white bg-neutral-900 w-full max-w-[800px] max-h-[95dvh] rounded flex flex-col backdrop:bg-black/50 mb-0"
+>
+	{#if $page.state.modal === 'stop'}
+		<StopModal {show_previous} time_format={time_format.value} stop={$page.state.data} />
 
-				{@render actions(
-					true,
-					's',
-					$page.state.data.id,
-					`Arrivals at ${$page.state.data.name}`,
-					stop_pins_rune
-				)}
-			{:else if $page.state.modal === 'route'}
-				<RouteModal route={$page.state.data} time_format={time_format.value} />
+		{@render actions(
+			true,
+			's',
+			$page.state.data.id,
+			`Arrivals at ${$page.state.data.name}`,
+			stop_pins_rune
+		)}
+	{:else if $page.state.modal === 'route'}
+		<RouteModal route={$page.state.data} time_format={time_format.value} />
 
-				{@render actions(
-					true,
-					'r',
-					$page.state.data.id,
-					`Alerts for ${$page.state.data.id}`,
-					route_pins_rune
-				)}
-			{:else if $page.state.modal === 'trip'}
-				<TripModal trip={$page.state.data} {show_previous} time_format={time_format.value} />
+		{@render actions(
+			true,
+			'r',
+			$page.state.data.id,
+			`Alerts for ${$page.state.data.id}`,
+			route_pins_rune
+		)}
+	{:else if $page.state.modal === 'trip'}
+		<TripModal trip={$page.state.data} {show_previous} time_format={time_format.value} />
 
-				{@render actions(
-					true,
-					't',
-					$page.state.data.id,
-					`${$page.state.data.route_id} Trip`,
-					trip_pins_rune
-				)}
-			{/if}
-		</div>
-	</div>
-{/if}
+		{@render actions(
+			true,
+			't',
+			$page.state.data.id,
+			`${$page.state.data.route_id} Trip`,
+			trip_pins_rune
+		)}
+	{/if}
+</dialog>
+<!-- </div>
+{/if} -->
 
 <!-- <style>
 	@keyframes spin {
