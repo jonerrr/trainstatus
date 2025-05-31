bd2e8f1ae27dfe50edcc81657b6f04b1c0e75030
Jonah <73760377+jonerrr@users.noreply.github.com>
2025-05-31 17:21:50 -0400

    improve settings modal

--- frontend/src/lib/Settings/Modal.svelte
+++ frontend/src/lib/Settings/Modal.svelte
@@ -3,9 +3,10 @@
 	import { onMount } from 'svelte';
 	import {
 		BookText,
-		GitBranch,
+		CodeXml,
 		Hourglass,
 		CircleX,
+		Info,
 		Settings,
 		Map,
 		ExternalLink,
@@ -35,22 +36,22 @@
 </div>
 
 <div
-	class="flex max-h-[60dvh] flex-col divide-y divide-neutral-800 overflow-auto border-y border-neutral-800 bg-neutral-950 text-base"
+	class="flex max-h-[80dvh] flex-col divide-y divide-neutral-800 overflow-auto border-y border-neutral-800 bg-neutral-950 text-base"
 >
 	<div class="p-4">
 		<div class="mb-3 flex items-center justify-between">
 			<div class="flex items-center gap-1">
 				<Hourglass class="size-5" />
-				<h3 class="text-lg font-medium">Custom Time</h3>
-				<h1 class="text-xs text-yellow-300">(Beta)</h1>
+				<h2 class="text-lg font-medium">Custom Time</h2>
+				<span class="text-xs text-yellow-300">(Beta)</span>
 			</div>
 		</div>
-		<p class="mb-3 text-sm text-neutral-400">
+		<p class="mb-3 pl-6 text-sm text-neutral-400">
 			View train and bus information from a specific time in the past. This allows you to see
 			historical train movements and delays for research or analysis purposes.
 		</p>
 
-		<div class="flex items-center gap-2">
+		<div class="flex items-center gap-2 pl-6">
 			<input
 				max={dayjs().format('YYYY-MM-DDTHH:mm')}
 				style="color-scheme: dark; font-size: 0.875rem"
@@ -83,11 +84,17 @@
 	</div>
 
 	<div class="p-4">
-		<h3 class="mb-3 text-lg font-medium">Resources</h3>
+		<div class="mb-3 flex items-center justify-between">
+			<div class="flex items-center gap-1">
+				<Info class="size-5" />
+				<h3 class="text-lg font-medium">Resources</h3>
+			</div>
+		</div>
+
 		<div class="flex flex-col gap-4">
 			<a
 				href="/charts{current_time.value ? `?at=${current_time.value}` : ''}"
-				class="active:scale-98 flex items-center gap-2 rounded-md p-2 transition-all duration-200 hover:bg-neutral-800/50 hover:text-emerald-400 focus:outline-none focus:ring-2 focus:ring-emerald-500/30 active:bg-neutral-800"
+				class="active:scale-98 flex items-center gap-2 rounded-md p-2 pl-6 transition-all duration-200 hover:bg-neutral-800/50 hover:text-emerald-400 focus:outline-none focus:ring-2 focus:ring-emerald-500/30 active:bg-neutral-800"
 			>
 				<div>
 					<div class="flex items-center gap-1">
@@ -97,24 +104,11 @@
 					<div class="text-xs text-neutral-400">Subway and Bus string lines</div>
 				</div>
 			</a>
-			<a
-				href="/api/docs"
-				target="_blank"
-				class="active:scale-98 flex items-center gap-2 rounded-md p-2 transition-all duration-200 hover:bg-neutral-800/50 hover:text-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-500/30 active:bg-neutral-800"
-			>
-				<div>
-					<div class="flex items-center gap-1">
-						<BookText class="size-5" />
-						<span>Api Documentation</span>
-						<ExternalLink class="size-4" />
-					</div>
-					<div class="text-xs text-neutral-400">Access Train Status data for your own projects</div>
-				</div>
-			</a>
+
 			<a
 				href="https://map.trainstat.us"
 				target="_blank"
-				class="active:scale-98 flex items-center gap-2 rounded-md p-2 transition-all duration-200 hover:bg-neutral-800/50 hover:text-rose-400 focus:outline-none focus:ring-2 focus:ring-rose-500/30 active:bg-neutral-800"
+				class="active:scale-98 flex items-center gap-2 rounded-md p-2 pl-6 transition-all duration-200 hover:bg-neutral-800/50 hover:text-rose-400 focus:outline-none focus:ring-2 focus:ring-rose-500/30 active:bg-neutral-800"
 			>
 				<div>
 					<div class="flex items-center gap-1">
@@ -125,14 +119,30 @@
 					<div class="text-xs text-neutral-400">A realtime map of buses in New York City</div>
 				</div>
 			</a>
+
 			<a
-				href="https://github.com/jonerrr/trainstatus"
+				href="/api/docs"
 				target="_blank"
-				class="active:scale-98 flex items-center gap-2 rounded-md p-2 transition-all duration-200 hover:bg-neutral-800/50 hover:text-green-400 focus:outline-none focus:ring-2 focus:ring-green-500/30 active:bg-neutral-800"
+				class="active:scale-98 flex items-center gap-2 rounded-md p-2 pl-6 transition-all duration-200 hover:bg-neutral-800/50 hover:text-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-500/30 active:bg-neutral-800"
 			>
 				<div>
 					<div class="flex items-center gap-1">
-						<GitBranch class="size-5" />
+						<BookText class="size-5" />
+						<span>Api Documentation</span>
+						<ExternalLink class="size-4" />
+					</div>
+					<div class="text-xs text-neutral-400">Access Train Status data for your own projects</div>
+				</div>
+			</a>
+
+			<a
+				href="https://github.com/jonerrr/trainstatus"
+				target="_blank"
+				class="active:scale-98 flex items-center gap-2 rounded-md p-2 pl-6 transition-all duration-200 hover:bg-neutral-800/50 hover:text-green-400 focus:outline-none focus:ring-2 focus:ring-green-500/30 active:bg-neutral-800"
+			>
+				<div>
+					<div class="flex items-center gap-1">
+						<CodeXml class="size-5" />
 						<span>Source Code</span>
 						<ExternalLink class="size-4" />
 					</div>
