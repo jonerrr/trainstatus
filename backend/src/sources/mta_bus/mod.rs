e7df00d1d88dd1ced2ecd27e3e351afdafff3472
Jonah <73760377+jonerrr@users.noreply.github.com>
2026-03-28 22:05:50 -0400

    refactor(oba): store agencies in one file

--- backend/src/sources/mta_bus/mod.rs
+++ backend/src/sources/mta_bus/mod.rs
@@ -1,3 +1,6 @@
 pub mod alerts;
 pub mod realtime;
 pub mod static_data;
+
+/// Used to fetch OBA data. Doesn't seem to ever change so its hardcoded
+pub const AGENCIES: [&str; 2] = ["MTABC", "MTA NYCT"];
