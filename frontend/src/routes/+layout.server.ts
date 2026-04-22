f197030c9d490e510f029448e0ceb298f656e0db
Jonah <73760377+jonerrr@users.noreply.github.com>
2026-04-22 10:31:06 -0400

    fix: separate load function into universal and server side

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -1,9 +1,11 @@
-import type { Route, Source, Stop } from '$lib/client';
+import type { Source } from '$lib/client';
 import { COOKIE_NAME, parse_sources, supported_sources } from '$lib/source_preferences.svelte';
 
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = async ({ fetch, url, cookies }) => {
+// lightweight server load that just reads the cookie and URL params and passes it to the universal load function.
+// This way the client-side caching actually works properly since data isn't only being fetched server-side
+export const load: LayoutServerLoad = async ({ url, cookies }) => {
 	const cookie_value = cookies.get(COOKIE_NAME);
 	let selected_sources = parse_sources(cookie_value);
 
@@ -19,41 +21,8 @@ export const load: LayoutServerLoad = async ({ fetch, url, cookies }) => {
 
 	const at = url.searchParams.get('at') ?? undefined;
 
-	const [stop_results, route_results] = await Promise.all([
-		Promise.all(
-			selected_sources.map(async (source) => ({
-				source,
-				data: (await (await fetch(`/api/v1/stops/${source}`)).json()) as Stop[]
-			}))
-		),
-		Promise.all(
-			selected_sources.map(async (source) => ({
-				source,
-				data: (await (await fetch(`/api/v1/routes/${source}`)).json()) as Route[]
-			}))
-		)
-	]);
-
-	const stops: App.PageData['stops'] = {};
-	const stops_by_id: App.PageData['stops_by_id'] = {};
-	for (const { source, data } of stop_results) {
-		stops[source] = data;
-		stops_by_id[source] = Object.fromEntries(data.map((stop) => [stop.id, stop]));
-	}
-
-	const routes: App.PageData['routes'] = {};
-	const routes_by_id: App.PageData['routes_by_id'] = {};
-	for (const { source, data } of route_results) {
-		routes[source] = data;
-		routes_by_id[source] = Object.fromEntries(data.map((route) => [route.id, route]));
-	}
-
 	return {
 		selected_sources,
-		stops,
-		routes,
-		stops_by_id,
-		routes_by_id,
 		at
 	};
 };
